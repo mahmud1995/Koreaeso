fdf968b7be800c7c38b93becd33ceadaaefa63cc
mahmud1995 <makhmudgo95@gmail.com>
2025-01-15 23:39:47 +0900

    Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+const ali: string = "hello";
+
+// axios = data fetch
+// react rooter = urganish
+// reactQuery - without GraphQL ishlatsa buladi
+// useEffect = useQuery
+
+const person: string = "Jony";
+const count: number = 100;
+console.log(ali, person, count);
